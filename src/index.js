f52cee88852ff103d9692a2693cc5b2d431e32d7
Andre <andredelima92@gmail.com>
2022-11-28 18:52:35 -0300

    update

--- src/index.js
+++ src/index.js
@@ -20,6 +20,8 @@ async function mainLoop() {
 
     const person = Person.generateInstanceFromString(answer);
     console.log("person", person.formatted(DEFAULT_LANG));
+    terminalController.updateTable(person.formatted(DEFAULT_LANG));
+
     return mainLoop();
   } catch (err) {
     console.error("Deu RUIM**", err);
